abad30c9a1531960970388e630d93612b0fd531a
Umair Adil <umair.adil@northbaysolutions.net>
2023-03-15 21:46:35 +0500

    - Updated list design

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,9 @@ import { Product, ProductElement } from './models/Product';
 import { useAppSelector, useAppDispatch } from './hooks';
 import Grid from '@mui/material/Grid';
 import Grid2 from '@mui/material/Unstable_Grid2';
-
+import Fab from '@mui/material/Fab';
 import React from 'react';
+import ShowDetailsIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -38,40 +39,41 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container spacing={1}>
-        <Grid xs={4}>
+      <Grid container spacing={0}>
+        <Grid xs={6}>
           <Item>
-          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <div>{products.map(p =>
-          <div key={p.id}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar alt="Remy Sharp" src={p.thumbnail} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={p.title}
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: 'inline' }}
-                      component="span"
-                      variant="body2"
-                      color="text.primary"
-                    >
-                      Ali Connors
-                    </Typography>
-                    {p.description}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </div>
-        )}</div>
-      </List>
+            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+              <div>{products.map(p =>
+                <div key={p.id}>
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar alt={p.title} src={p.images?.at(0)} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={p.title}
+                      secondary={
+                        <React.Fragment>
+                          <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="caption"
+                            color="text.primary"
+                          >{p.description}
+                          </Typography>
+                        </React.Fragment>
+                      }
+                    />
+                    <Fab size="medium" color="primary" aria-label="add">
+                      <ShowDetailsIcon />
+                    </Fab>
+                  </ListItem>
+                </div>
+              )}</div>
+            </List>
           </Item>
         </Grid>
       </Grid>
-      
+
     </div>
   );
 }
